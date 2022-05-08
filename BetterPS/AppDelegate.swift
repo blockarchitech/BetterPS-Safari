ec3c0dc42ab204b951e844e9ae86fe5366f391ad
BlockArchitech <gibbiemonster4@gmail.com>
2022-05-08 11:17:56 -0400

    GitHub Upload 1/2

---
+++ BetterPS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BetterPS
+//
+//  Created by blockarchitech on 5/8/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
